f1c6e89f1ab558ff3b026ae8b2deb75a7aba0518
Florian Rodriguez <florian@negative-network.com>
2014-12-03 17:34:39 +0100

    Reorganization assets

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,34 +16,38 @@ module.exports = function(grunt) {
 			}
 		},
 
-		copy: {
-			main: {
-				expand: true,
-				cwd: '../FoundationPress/bower_components',
-				src: [
-					'modernizr/modernizr.js',
-					'jquery/dist/jquery.min.js',
-					'foundation/js/foundation.min.js'
-				],
-				dest: 'js'
-			},
-		},
+		// copy: {
+		// 	main: {
+		// 		expand: true,
+		// 		cwd: '../FoundationPress/bower_components',
+		// 		src: [
+		// 			'modernizr/modernizr.js',
+		// 			'jquery/dist/jquery.min.js',
+		// 			'foundation/js/foundation.min.js'
+		// 		],
+		// 		dest: 'js'
+		// 	},
+		// },
 
 		uglify: {
 
-			base: {
-				files: [{
-					src: [
-						'js/modernizr/modernizr.js',
-						'js/jquery/dist/jquery.min.js',
-						'js/foundation/js/foundation.min.js',
-					],
-					dest: 'js/base.min.js'
-				}]
-			},
+			// base: {
+			// 	files: [{
+			// 		src: [
+			// 			'../FoundationPress/bower_components/modernizr/modernizr.js',
+			// 			'../FoundationPress/bower_components/jquery/dist/jquery.min.js',
+			// 		],
+			// 		dest: 'js/base.min.js'
+			// 	}]
+			// },
 			custom: {
 				files: [{
 					src: [
+						// 'js/custom/*.js',
+						'../FoundationPress/bower_components/foundation/js/foundation.min.js',
+						// add your plugin here. Ex:
+						// 'js/custom/imagesloaded.pkgd.min.js',
+						// 'js/custom/isotope.pkgd.min.js',
 						'js/custom/init-foundation.js',
 						'js/custom/app.js',
 					],
@@ -87,8 +91,15 @@ module.exports = function(grunt) {
 				files: [
 					// Anytime css is edited or compiled by sass, trigger the livereload on those files
 					'css/app.css',
+					// In parent theme also decomment below
+					// '../FoundationPress/scss/**/*.scss',
+					
 					// Or a js file
 					'js/custom.min.js',
+					// '../**/*.js'
+					
+					// Or a php file
+					// '../**/*.php','./**/*.php',
 				]
 			}
 		}
@@ -101,6 +112,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	// grunt.registerTask('build', ['copy', 'uglify', 'concat', 'sass']);
-	grunt.registerTask('build', ['copy', 'uglify', 'sass']);
+	grunt.registerTask('build', ['uglify', 'sass']);
 	grunt.registerTask('default', [ 'build','watch']);
 }
--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -3,38 +3,22 @@
 function FoundationPress_scripts() {
 
     // deregister the jquery version bundled with wordpress
-    wp_deregister_script( 'jquery' );
+    wp_deregister_script('jquery');
 
-    // register scripts
-    // wp_register_script( 'modernizr', get_template_directory_uri() . '/js/modernizr/modernizr.min.js', array(), '1.0.0', false );
-    // wp_register_script( 'jquery', get_template_directory_uri() . '/js/jquery/dist/jquery.min.js', array(), '1.0.0', false );
-    // wp_register_script( 'foundation', get_template_directory_uri() . '/js/app.js', array('jquery'), '1.0.0', true );
-    
-    wp_register_script( 'base', get_stylesheet_directory_uri() . '/js/base.min.js', array(), '1.0.0', false );
-    // wp_register_script( 'foundation', get_stylesheet_directory_uri() . '/js/foundation/js/foundation.min.js', array('base'), '1.0.0', true );
-    wp_register_script( 'custom', get_stylesheet_directory_uri() . '/js/custom.min.js', array('base'), '1.0.0', true );
-    
-    // wp_register_script( 'app', get_stylesheet_directory_uri() . '/js/app.min.js', array(), '1.0.0', false );
+	// register scripts
+	wp_register_script( 'modernizr', get_template_directory_uri() . '/js/modernizr/modernizr.min.js', array(), '1.0.0', false );
+    wp_register_script( 'jquery', get_template_directory_uri() . '/js/jquery/dist/jquery.min.js', array(), '1.0.0', false );
+	wp_register_script( 'custom', get_stylesheet_directory_uri() . '/js/custom.min.js', array('jquery','modernizr'), '1.0.0', true );
 
-    // enqueue scripts
-    // wp_enqueue_script('modernizr');
-    // wp_enqueue_script('jquery');
-    // wp_enqueue_script('foundation');
-    
-    wp_enqueue_script('base');
-    // wp_enqueue_script('foundation');
-    wp_enqueue_script('custom');
-    
-    // wp_enqueue_script('app');
-    
+	// enqueue scripts
+	wp_enqueue_script('custom');
+
     if (getenv('WP_ENV') == 'development')
     {
-        wp_enqueue_script('init-livereload', get_template_directory_uri() . '/js/init-livereload.js', array('base'), false, true);
+    	wp_register_script( 'init-livereload', get_template_directory_uri() . '/js/init-livereload.js', array('jquery'), '1.0.0', true );
+    	wp_enqueue_script('init-livereload');
     }
-
 }
 
-add_action( 'wp_enqueue_scripts', 'FoundationPress_scripts' );
-
-
+add_action('wp_enqueue_scripts', 'FoundationPress_scripts');
 ?>
